1941586b92a7985cf8ec80277472bbb45a3f0633
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2024-12-10 19:47:00 -0300

    implementando refresh token

--- backend/src/controllers/refreshController.js
+++ backend/src/controllers/refreshController.js
@@ -1,6 +1,26 @@
-
+const jwt = require('jsonwebtoken')
 
 
 exports.refreshToken = (req, res) => {
+    try {
+
+        const refreshToken = req.cookies.refreshToken || req.headers.authorization?.replace('Bearer ', '')
+
+        if (!refreshToken || !refreshTokenContainer.include(refreshToken)) {
+            return res.status(403).json({ error: 'Refresh Token inválido!' })
+        }
+
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+            if (err) return res.status(403).json({ error: 'refresh token inválido!' })
+
+            const newAcessToken = jwt.sign({ username: user.name }, process.env.ACESS_TOKEN, { expiresIn: '15min' })
+
+            res.status(200).json({ accessToken: newAcessToken })
+        })
+
+
+    } catch (error) {
+
+    }
 
 }
