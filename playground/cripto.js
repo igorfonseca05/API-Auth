8ff75aa7dcf311b27d43dd2e3501354f2198c557
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2025-01-14 21:06:52 -0300

    fazendo alguns testes

--- playground/cripto.js
+++ playground/cripto.js
@@ -1,8 +1,14 @@
 
 const crypto = require('crypto')
 
+
+
+/**No código abaixo estou usando o crypto para criar um par de chaves publica e privada
+ * para estudar como funciona a criptografia assimétrica que usa a chave pública para
+ * escrever 
+ */
 const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 1024,
+    modulusLength: 2048,
     publicKeyEncoding: {
         type: 'spki',
         format: 'pem'
@@ -12,5 +18,7 @@ const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
         format: 'pem'
     }
 })
+
+
 
 console.log(privateKey, publicKey)
